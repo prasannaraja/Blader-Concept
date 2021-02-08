d371545025689a9bcc77a02d1547ebd63fa55227
Prasanna Raja <prasannaraja@msn.com>
2021-02-08 20:25:12 +0530

    new blader componet reference udpated

--- src/app/blader/index.ts
+++ src/app/blader/index.ts
@@ -2,6 +2,6 @@ export * from './models';
 export * from './blade/blade.component';
 export * from './bladeManager.service';
 export * from './bladeRegistry.service';
-export * from './blader.component';
+export * from './blader/blader.component';
 
 export * from './blader.module';
