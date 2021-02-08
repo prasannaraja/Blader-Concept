9a24790d5a55d6e68573f44dbb747fd80f7cac50
Prasanna Raja <prasannaraja@msn.com>
2021-02-08 20:34:48 +0530

    model & index moved to shared

---
+++ src/app/shared/blader/index.ts
@@ -0,0 +1,7 @@
+export * from './models';
+export * from './blade/blade.component';
+export * from './bladeManager.service';
+export * from './bladeRegistry.service';
+export * from './blader/blader.component';
+
+export * from './blader.module';
