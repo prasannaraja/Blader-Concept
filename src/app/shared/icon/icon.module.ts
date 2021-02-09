10bff2e71a98b324fb69036a56b40d5438f9327a
Prasanna Raja <prasannaraja@msn.com>
2021-02-10 00:09:23 +0530

    icon component

--- src/app/shared/icon/icon.module.ts
+++ src/app/shared/icon/icon.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { IconComponent } from './icon.component';
+
 
 @NgModule({
+  declarations: [],
   imports: [
     CommonModule
-  ],
-  declarations: [
-    IconComponent
-  ],
-  exports: [
-    IconComponent
   ]
 })
 export class IconModule { }
