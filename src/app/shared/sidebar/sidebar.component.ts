b818038b6176da643b586591aac78b8af577b232
Prasanna Raja <prasannaraja@msn.com>
2021-02-11 23:37:53 +0530

    sidebar component updated to proper component

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,29 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'tw-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css'],
   host: { 'class': 'sidebar' },
-  template: `
-   <div class="sidebar-header">
-     <a routerLink="/home" i18n>Blader</a>
-   </div>
-   <div class="sidebar-content">
-     <ul class="sidebar-nav">
-      <li routerLinkActive="active">
-        <a routerLink="/home">Home</a>
-      </li>
-      <li routerLinkActive="active">
-        <a routerLink="/list">List</a>
-      </li>
-      <li routerLinkActive="active">
-        <a routerLink="/detail">Detail</a>
-      </li>
-      <li routerLinkActive="active">
-        <a routerLink="/blader/entry">Blader</a>
-      </li>
-     </ul>
-   </div>
-   <div class="sidebar-footer"></div>`
 })
-export class SidebarComponent {
-}
+export class SidebarComponent   { }
